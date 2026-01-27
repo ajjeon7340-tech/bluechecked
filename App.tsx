b766b5592e9ca30228a960efc78cf9bbfdf7f833
aj <ajeon7340@gmail.com>
2026-01-26 21:44:39 -0600

    fix

--- App.tsx
+++ App.tsx
@@ -81,7 +81,7 @@ function App() {
 
     window.addEventListener('popstate', handlePopState);
 
-    console.log("Bluechecked App Version: 3.6.20");
+    console.log("Bluechecked App Version: 3.6.21");
     console.log("Backend Connection:", isBackendConfigured() ? "✅ Connected to Supabase" : "⚠️ Using Mock Data");
     loadCreatorData();
     
